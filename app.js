25968dc63f742a435f7f24001608ec9a232b9fc8
Scylol <djd3dobbin@gmail.com>
2017-04-03 14:11:09 -0400

    added try block

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 function yearOfBirth(age){
-	return 2017 - age;
+	if(age < 0) {
+		throw new Error("Age can not be negative");
+	}
+
+	else {
+		return 2017 - age;
+	}
+	
 }
 
 function whoAmI(name, age) {
+	if(typeof age !== 'number' && typeof name !== 'string') {
+		throw new Error("Please enter a number in the age parameter and a string in the name parameter! ")
+	}
+	
+	if(typeof age !== 'number') {
+		throw new Error("Please enter a number in the age parameter!");
+	}
+	if (typeof name !== 'string') {
+		throw new Error("Please enter a string in the name parameter!");
+	}
 	let yob = yearOfBirth(age);
+	
 	console.log(`My name is ${name} and I'm ${age} years old.`);
 	console.log(`I was born in ${yob}.`);
 }
 
-whoAmI("Chris", 29);
+try {
+	whoAmI("Hello", "hi");
+}
+catch(e) {
+	console.log(e.message);
+}
+
 
